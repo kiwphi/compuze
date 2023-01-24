7412b30551ce3cabb1abbbd428e6c0e9e49fd361
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-01-24 20:13:28 +0200

    add loading state to favorite button

--- frontend/src/components/item/favorite-button.js
+++ frontend/src/components/item/favorite-button.js
@@ -4,28 +4,33 @@ import { deleteRequest, getRequest, postRequest } from '../../util/api-requests'
 const FavoriteButton = ({ itemId }) => {
   // state
   const [isFavorite, setIsFavorite] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   // api call - toggle favorite
   const toggleFavorite = async () => {
+    setIsLoading(true);
     const json = isFavorite
       ? await deleteRequest(`${process.env.NEXT_PUBLIC_API_URL}/items/${itemId}/favorites`)
       : await postRequest(`${process.env.NEXT_PUBLIC_API_URL}/items/${itemId}/favorites`);
     if (json.success) {
       setIsFavorite(!isFavorite);
+      setIsLoading(false);
     }
   };
 
   // api call - check if item is in favorites
   useEffect(() => {
     (async () => {
+      setIsLoading(true);
       const json = await getRequest(`${process.env.NEXT_PUBLIC_API_URL}/items/${itemId}/favorites`);
       setIsFavorite(json.success);
+      setIsLoading(false);
     })();
   }, [itemId]);
 
   // render
   return (
-    <button className={`big-btn ${isFavorite ? 'pink-btn' : 'blue-btn'}`} onClick={toggleFavorite}>
+    <button disabled={isLoading} className={`big-btn ${isFavorite ? 'pink-btn' : 'blue-btn'}`} onClick={toggleFavorite}>
       {isFavorite ? 'Remove from favorites' : 'Add to favorites'}
     </button>
   );
