f040adec79e3cb1ed7573f1eb046ac7bfe483c0e
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-02-07 19:22:51 +0200

    add new items to fake-data populator

--- backend/src/util/db-populator.js
+++ backend/src/util/db-populator.js
@@ -1,18 +1,24 @@
 import { createUser } from '../services/auth-service.js';
 import { createItem } from '../services/item-service.js';
 import { db } from './db-connection.js';
-import { FAKE_ITEMS, FAKE_USER } from './fake-data.js';
+import { FAKE_ITEMS, FAKE_USERS } from './fake-data.js';
 
 export async function populateDatabase() {
     const items = await db('items');
 
     if (!items.length) {
-        // insert fake user into database
-        await createUser(FAKE_USER);
-
-        // insert fake items into database
-        FAKE_ITEMS.forEach(async (item) => {
-            createItem(item);
-        });
+        await createUsers();
+        await createItems();
     }
+}
+
+async function createUsers() {
+    FAKE_USERS.forEach(async (user) => {
+        await createUser(user);
+    });
+}
+async function createItems() {
+    FAKE_ITEMS.forEach(async (item) => {
+        await createItem(item);
+    });
 }
